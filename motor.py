3cebc6b5b1c9ea39edf20ee95e86cddedf6f8994
Terry <terry@agapitos.com.au>
2017-04-05 19:26:08 +1000

    (potential) final changes?

--- motor.py
+++ motor.py
@@ -1,10 +1,10 @@
 from ev3dev.ev3 import *
 from time import sleep, time
 
-ROBOT_DISTANCE = 100 # Distance between one robot and the other.
+ROBOT_DISTANCE = 700 # Distance between one robot and the other.
 
-rightMotor = LargeMotor(OUTPUT_A)
-leftMotor = LargeMotor(OUTPUT_D)
+rightMotor = LargeMotor(OUTPUT_A); assert rightMotor.connected
+leftMotor = LargeMotor(OUTPUT_D); assert leftMotor.connected
 sensorMotor = Motor(OUTPUT_C); assert sensorMotor.connected
 us = UltrasonicSensor(INPUT_1); assert us.connected
 btn = Button()
@@ -24,6 +24,7 @@ def startup():
 	while not backspace_pressed:
 		backspace_pressed = btn.backspace
 
+	sleep(1)
 	return time()
 
 def locate_first():
@@ -39,9 +40,9 @@ def locate_first():
 			sensorMotor.stop()
 			pos = sensorMotor.position
 			if pos >= 0:
-				return sensorMotor.position, 1
+				return sensorMotor.position+30, 1
 			else:
-				return abs(sensorMotor.position), -1
+				return abs(sensorMotor.position)+30, -1
 		if not sensorMotor.state:
 			print("Turning around")
 			sensorMotor.run_to_abs_pos(position_sp=-270, speed_sp=250)
@@ -53,8 +54,8 @@ def locate_subsequent():
 	Different to first locate since now we are turning while locating 
 	-- not using sensorMotor
 	"""
-	leftMotor.run_timed(time_sp=10000, speed_sp=-550) # Perhaps change dir depending on
-	rightMotor.run_timed(time_sp=10000, speed_sp=550) # last location seen?
+	leftMotor.run_timed(time_sp=10000, speed_sp=-1050) # Perhaps change dir depending on
+	rightMotor.run_timed(time_sp=10000, speed_sp=1050) # last location seen?
 	while True:
 		distance = us.value()
 		if distance < ROBOT_DISTANCE:
@@ -68,10 +69,10 @@ def turn(pos, direction):
 
 	Determined through the position of the sensorMotor when it found the other robot.
 	"""
-	leftMotor.run_to_abs_pos(position_sp=pos*direction)
-	rightMotor.run_to_abs_pos(position_sp=-1*pos*direction)
-	while any(m.state for m in (leftMotor, rightMotor)):
-		continue # Let the motors completely turn before doing drive()
+	leftMotor.run_to_abs_pos(position_sp=pos*direction, speed_sp=1000)
+	rightMotor.run_to_abs_pos(position_sp=-1*pos*direction, speed_sp=1000)
+	sleep(0.5)
+	input("Did it turn well?")
 
 def drive():
 	"""
@@ -96,12 +97,21 @@ def main():
 	rightMotor.position = 0 # Defining the starting point as 0.
 	init = startup()
 	enemy_pos, direction = locate_first()
-	sensorMotor.run_to_abs_pos(position_sp=10) # Reset sensorMotor back to front.
-	sleep(3-(time()-init))
-	turn(2.5*enemy_pos, direction) # 2.5 MIGHT NEED CHANGING DEPENDING ON SURFACE OF PLAYING AREA
+	print(enemy_pos, direction)
+	sensorMotor.run_to_abs_pos(position_sp=0) # Reset sensorMotor back to front.
+	# Can we move ^ into locate_first?
+	try:
+		sleep(2-(time()-init))
+	except ValueError: # It took longer than 3 seconds
+		pass
+	print("done sleeping")
+	turn(3*enemy_pos, direction) # 2.5 MIGHT NEED CHANGING DEPENDING ON SURFACE OF PLAYING AREA
+	#sleep(0.5)
+	print("done turning")
 	while True:
 		try: # Test this.
-			if drive() == "lost":
+			res = drive()
+			if res == "lost":
 				locate_subsequent()
 		except KeyboardInterrupt:
 			leftMotor.stop()
